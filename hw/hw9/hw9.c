677c6b1839f0ff155300e339d4b50265b31da944
CAYYY9866 <frenkenss1@gmail.com>
2017-12-14 05:35:41 +0800

    completed

--- hw/hw9/hw9.c
+++ hw/hw9/hw9.c
@@ -1,28 +1,61 @@
 #include<stdio.h>
 #include<string.h>
-#define MaxPat 100 MaxLine 1000
+#define MaxPat 100 
+#define MaxLine 1000
 
 void usage(){
 
-	puts("please input a correct option
-		  -c: print pattern counts
-		  -m: find lines containing a pattern
-		  -v: find lines not containing a pattern");
+	printf("please input a correct option\n-c: print pattern counts\n-m: find lines containing a pattern\n-v: find lines not containing a pattern\n");
 
 }
-int main(int argc,char *argv){
+
+int mystrstr(char *s,char *find)
+{
+	char c,sc;
+	int len,n=0;
+	if((c=*find++)!=0){
+		len = strlen(find);
+		do{
+			do{
+				if((sc=*s++)=='\n')break;
+			}while(sc!=c);
+			if(strncmp(s,find,len)==0)n++;
+		}while(sc!='\n');
+	}
+	return n;
+}
+int main(int argc,char **argv){
 
 	char pat[MaxPat],line[MaxLine];
 	int m=0,v=0,c=0;
 	char *x="-m",*y="-v",*z="-c";
 	if(argc!=3)usage();
-	if(!strcmp(argv[1],x))m=1;
-	else if(!strcmp(argv[1],y))v=1;
-	else if(!strcmp(argv[1],z))c=1;
-	else usage();
-	
-	pat=argv[2];
-	
+	else
+	{
+		if(!strcmp(argv[1],x))m=1;
+		else if(!strcmp(argv[1],y))v=1;
+		else if(!strcmp(argv[1],z))c=1;
+		else usage();
 
+		strcpy(pat,argv[2]);
+		int count = 0,n;
+		while(fgets(line,MaxLine,stdin))
+		{
+			if((n=mystrstr(line,pat)))
+			{
+				count+=n;
+				if(m)printf("%s",line);
+			}
+			else if(v)
+			{
+				printf("%s",line);
+			}
+		}
+
+		if(c)
+		{
+			printf("%s: %d\n",pat,count);
+		}
+	}
 
 return 0;}
