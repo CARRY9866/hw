29a635fc00f179fab13391b93adff6e99ed286c9
User U03530035 <u03530035@csie2.cs.ccu.edu.tw>
2017-12-14 02:26:39 +0800

    usage

--- hw/hw9/hw9.c
+++ hw/hw9/hw9.c
@@ -1,9 +1,28 @@
 #include<stdio.h>
-int main(){
-    printf("test\n");
+#include<string.h>
+#define MaxPat 100 MaxLine 1000
 
+void usage(){
 
+	puts("please input a correct option
+		  -c: print pattern counts
+		  -m: find lines containing a pattern
+		  -v: find lines not containing a pattern");
 
+}
+int main(int argc,char *argv){
+
+	char pat[MaxPat],line[MaxLine];
+	int m=0,v=0,c=0;
+	char *x="-m",*y="-v",*z="-c";
+	if(argc!=3)usage();
+	if(!strcmp(argv[1],x))m=1;
+	else if(!strcmp(argv[1],y))v=1;
+	else if(!strcmp(argv[1],z))c=1;
+	else usage();
+	
+	pat=argv[2];
+	
 
 
 return 0;}
